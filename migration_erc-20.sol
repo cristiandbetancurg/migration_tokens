9d4bf960b6c48f5f9d78f6d3e8cdcb0f8524127f
Cristian Betancur <34930340+cristiandbetancurg@users.noreply.github.com>
2024-05-20 11:14:02 -0400

    Create migration_erc-20.sol
    This smart contract allows $RFKJ token holders to exchange their tokens for $BOBBY tokens. The contract verifies the authenticity of the $RFKJ tokens and issues the corresponding amount of $BOBBY tokens to each holder.

---
+++ migration_erc-20.sol
@@ -0,0 +1,23 @@
+Solidity
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RFKJMigration is ERC20 {
+    address public RFKJAddress; // Dirección del token $RFKJ
+    uint256 public RFKJToBOBBYRatio = 1; // Ratio de intercambio (1 $RFKJ = 1 $BOBBY)
+
+    constructor(string memory name, string memory symbol, address _RFKJAddress) ERC20(name, symbol) {
+        RFKJAddress = _RFKJAddress;
+    }
+
+    function migrateRFKJToBOBBY(uint256 amount) public {
+      
+        // Transferir tokens $RFKJ al contrato
+        ERC20(RFKJAddress).transferFrom(msg.sender, address(this), amount);
+
+        // Minting tokens $BOBBY al holder
+        _mint(msg.sender, amount * RFKJToBOBBYRatio);
+    }
+}
